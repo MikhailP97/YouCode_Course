86947b4237ee15025e8474e6311b70d909d1f831
Mitch-Aries <mykhaylopelykh@gmail.com>
2024-10-08 15:45:42 +0200

    #5 - Add course users list

---
+++ app/admin/courses/[courseId]/course.query.ts
@@ -0,0 +1,55 @@
+import { prisma } from "@/app/lib/prisma";
+
+export const getCourse = async ({
+    courseId,
+    userId,
+    userPage,
+}: {
+    courseId: string;
+    userId: string;
+    userPage: number;
+}) => {
+    const courses = await prisma.course.findUnique({
+        where: {
+            creatorId: userId,
+            id: courseId,
+        },
+        select: {
+            id: true,
+            image: true,
+            name: true,
+            presentation: true,
+            users: {
+                take: 2,
+                skip: Math.max(0, userPage * 2),
+                select: {
+                    canceledAt: true,
+                    id: true,
+                    user: {
+                        select: {
+                            email: true,
+                            id: true,
+                            image: true,
+                        },
+                    },
+                },
+            },
+            _count: {
+                select: {
+                    lessons: true,
+                    users: true,
+                },
+            },
+        },
+    });
+    const users = courses?.users.map((user) => {
+        return {
+            canceled: user.canceledAt ? true : false,
+            ...user.user,
+        };
+    });
+    return {
+        ...courses,
+        users,
+    };
+};
--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,7 +1,7 @@
 import { Layout, LayoutContent, LayoutHeader, LayoutTitle } from '@/app/components/layout/layout'
 import { Avatar, AvatarFallback, AvatarImage } from '@/app/components/ui/avatar'
 import { Card, CardContent } from '@/app/components/ui/card'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/app/components/ui/table'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/app/components/ui/table'
 import { Typography } from '@/app/components/ui/typography'
 import { getAuthSession, getRequiredAuthSession } from '@/app/lib/auth'
 import { prisma } from '@/app/lib/prisma'
@@ -11,7 +11,11 @@ import React, { useEffect, useState } from 'react'
 export default async function page() {
     const session = await getRequiredAuthSession()
 
-    const courses = await prisma.course.findMany({ where: { authorId: session?.user.id } })
+    const courses = await prisma.course.findMany({
+        where: {
+            creatorId: session.user.id,
+        },
+    });
 
     return (
         <Layout>
